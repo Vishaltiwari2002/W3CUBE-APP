f1a440573d26630a215d52797a8816190a70983b
Vishal_Tiwari <vishal1111202@gmail.com>
2024-06-08 00:02:47 +0530

    1st commit

---
+++ src/component/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { IoIosMenu, IoMdClose } from "react-icons/io";
+
+export default function Header() {
+  return (
+    <div className="bg-red-700">
+      <div className="max-w-[1240px] py-[15px] items-center flex justify-between mx-auto">
+        <div className="text-3xl font-bold">WsCube Tech</div>
+        <IoIosMenu className="text-white text-3xl md:hidden block" />
+        {/* <IoMdClose className="text-white text-3xl md:hidden block" /> */}
+        <ul className="md:flex text-white gap-10">
+          <li>Home</li>
+          <li>Company</li>
+          <li>Resources</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
